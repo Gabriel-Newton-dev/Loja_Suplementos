53caada0ad081bd84192dbbb1dee761068bcabcb
Gabriel <gabriel@MacBook.local>
2022-12-03 19:10:37 -0800

    Criacao de struct para lista de produtos

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"text/template"
 )
 
+type Produtos struct {
+	Nome       string
+	Descricao  string
+	Valor      float64
+	Quantidade int
+}
+
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
@@ -15,5 +22,13 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+	Produto := []Produtos{
+		{"Creatina Universal", "A melhor creatina do mercado", 119.89, 10},
+		{"HGH", "Otimizador de hormonio GH", 350.99, 5},
+		{"Whey Protein", "O whey protein tem alta concentração de cálcio", 59.99, 10},
+		{"Albumina", "Clara de ovo", 49.99, 50},
+		{"Ômega 3", "Multivitaminíco", 32.99, 30},
+		{"Natubolic", "Suplemento", 91.99, 4},
+	}
+	temp.ExecuteTemplate(w, "Index", Produto)
 }
