06f53d70b12f2d232b276eca6c99a334837b361c
Gabriel <gabriel@MacBook.local>
2022-12-13 22:00:42 -0800

    Modificado o go.mod

--- main.go
+++ main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
-	"modulos/models"
+	//"modulos/models"
 	"net/http"
 	"text/template"
+
+	"github.com/Gabriel-Newton-dev/models"
 )
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
-	http.HandleFunc("/", index)
+	http.HandleFunc("/", Index)
 	http.ListenAndServe(":8000", nil)
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-	todosOsProdutos := models.BuscaTodosOsProdutos
+func Index(w http.ResponseWriter, r *http.Request) {
+	todosOsProdutos := models.BuscaTodosOsProdutos()
 	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
 
 }
