7b30dac8a751c88a0885fbb4edaff6e5f4b2bdff
Gabriel <gabriel@MacBook.local>
2022-12-17 12:55:16 -0800

    Criacao da funcao inserir novos produtos

--- controllers/produtos.go
+++ controllers/produtos.go
@@ -2,7 +2,9 @@ package controllers
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/Gabriel-Newton-dev/models"
 )
@@ -17,4 +19,26 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 func New(w http.ResponseWriter, r *http.Request) {
 	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		valor := r.FormValue("valor")
+		quantidade := r.FormValue("quantidade")
+
+		valorConvertido, err := strconv.ParseFloat(valor, 64)
+		if err != nil {
+			log.Println("Erro na conversão do valor:", err)
+		}
+
+		quantidadeConvertida, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversão da quantidade")
+		}
+
+		models.CriarNovoProduto(nome, descricao, valorConvertido, quantidadeConvertida)
+	}
+	http.Redirect(w, r, "/", 301)
 }
